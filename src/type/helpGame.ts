c643e68390c7e996ff8fa78fdf2701f2767fc233
AntonFio <anton_fiolkovski@mail.ru>
2023-02-08 22:08:17 +0300

    fix: markup

--- src/type/helpGame.ts
+++ src/type/helpGame.ts
@@ -1,59 +1,125 @@
-const help = () => {
+const wrapperBlokHelp = () => {
   const block = `
-  <tr>
-      <td class="td"></td>
-      <td class="td"></td>
-      <td class="td"></td>
-      <td class="td help-life"></td>
-  </tr>
-  <tr>
-      <td rowspan="2" class="td help-player" data-click="true">
-      <div class="test"><div>
-      </td>
-      <td class="td"></td>
-      <td class="td"></td>
-      <td class="td"></td>
-  </tr>
-  <tr>
-      <td class="td"></td>
-      <td class="td"></td>
-      <td class="td"></td>
-  </tr>
-  <tr>
-      <td class="td"></td>
-      <td class="td"></td>
-      <td class="td"></td>
-      <td class="td help-settings"></td>
-  </tr>
-  `
+  <div class="colum-block">
+  <div class="col col1-1"></div>
+  <div class="col col1-2">
+    <img src="./catalog-img/male-player.png" style="width: 75%;" alt="#">
+    <div class="fon-text">движение вверх</div>
+  </div>
+  <div class="col col1-3"></div>
+  <div class="col col1-4"></div>
+</div>
+<div class="colum-block">
+    <div class="col">
+    </div>
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"></div>
+</div>
+<div class="colum-block3">
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"></div>
+</div>
+<div class="colum-block4">
+    <div class="col col4-1">
+        <div class="img-heart">
+          <img src="./catalog-img/heart.png" alt="#">
+          <img src="./catalog-img/heart.png" alt="#">
+          <img src="./catalog-img/heart.png" alt="#">
+        </div>
+        <span class="fon-text">Враг</span>
+    </div>
+    <div class="col ">
+      <img src="./catalog-img/enemy-1.png" style="width: 30%;" alt="#">
+      <span class="fon-text">Враг</span>
+    </div>
+    <div class="col"></div>
+    <div class="col col4-4">
+      <span class="fon-text">настройки</span>
+    </div>
+</div>`
 
-  const saction = <HTMLElement>document.createElement('table');
-    saction.classList.add('help-block')
+  const saction = <HTMLElement>document.createElement('div');
+    saction.classList.add('wrapper-blok-help')
     saction.innerHTML = block;
     document.body.appendChild(saction);
 }
-help();
+wrapperBlokHelp();
+
+const wrapperBlokHelpCopy = () => {
+  const block = `  <div class="colum-block">
+  <div class="col col-copy col1-1"></div>
+  <div class="col col-copy col1-2 help-player" data-click="true">
+  </div>
+  <div class="col col-copy col1-3"></div>
+  <div class="col col-copy col1-4"></div>
+</div>
+<div class="colum-block">
+    <div class="col col-copy"></div>
+    <div class="col col-copy"></div>
+    <div class="col col-copy"></div>
+    <div class="col col-copy"></div>
+</div>
+<div class="colum-block3">
+    <div class="col col-copy"></div>
+    <div class="col col-copy"></div>
+    <div class="col col-copy"></div>
+    <div class="col col-copy"></div>
+</div>
+<div class="colum-block4">
+    <div class="col col-copy col4-1  help-life">
+    </div>
+    <div class="col col-copy help-emeny">
+    </div>
+    <div class="col col-copy"></div>
+    <div class="col col-copy col4-4  help-settings"></div>
+</div>`
+
+  const saction = <HTMLElement>document.createElement('div');
+    saction.classList.add('wrapper-blok-help-copy')
+    saction.innerHTML = block;
+    document.body.appendChild(saction);
+}
+wrapperBlokHelpCopy();
 
 function clickBlockHelp() {
-  const tdBlock = document.querySelectorAll('.td')
-  const helpPlayer = document.querySelector('.help-player') as HTMLElement 
+  const divBlock = document.querySelectorAll('.col-copy')
+  const helpPlayer = document.querySelector('.help-player') as HTMLElement
   const helpLife = document.querySelector('.help-life') as HTMLElement
   const helpSettings = document.querySelector('.help-settings') as HTMLElement
-  
+  const helpEmeny = document.querySelector('.help-emeny') as HTMLElement
+  helpPlayer.style.background = 'none'
+  helpPlayer.style.cursor= 'pointer';
+  divBlock.forEach(item => {
 
-  tdBlock.forEach(item => {
     item.addEventListener('click', () => {
       if (item.classList.contains('help-player') && helpPlayer.dataset.click === 'true') {
-        helpPlayer.style.background = 'rgb(0, 0, 0, 0.8)'
-        helpLife.style.background = 'none'
-        helpLife.setAttribute('data-bollean', 'true')
+        helpLife.setAttribute('data-click', 'true')
+        helpPlayer.style.background = 'rgb(0, 0, 0, 0.5)'
         helpPlayer.dataset.click = 'false'
-      } else if (item.classList.contains('help-life') && helpLife.dataset.bollean === 'true') {
-        helpLife.style.background = 'rgb(0, 0, 0, 0.8)'
+        helpLife.style.background = 'none'
+        helpLife.style.cursor = 'pointer';
+        helpPlayer.style.cursor= 'auto';
+      } else if (item.classList.contains('help-life') && helpLife.dataset.click === 'true') {
+        helpEmeny.setAttribute('data-click', 'true')
+        helpLife.style.background = 'rgb(0, 0, 0, 0.5)'
+        helpEmeny.style.background = 'none'
+        helpLife.dataset.click = 'false'
+        helpEmeny.style.cursor = 'pointer';
+        helpLife.style.cursor= 'auto';
+      }else if (item.classList.contains('help-emeny') && helpEmeny.dataset.click === 'true') {
         helpSettings.style.background = 'none'
+        helpSettings.setAttribute('data-click', 'true')
+        helpEmeny.style.background = 'rgb(0, 0, 0, 0.5)'
+        helpSettings.style.cursor = 'pointer';
+        helpEmeny.style.cursor= 'auto';
       } else if (item.classList.contains('help-settings')) {
-        helpSettings.style.background = 'rgb(0, 0, 0, 0.8)'
-      } 
+        helpSettings.style.background = 'rgb(0, 0, 0, 0.5)'
+        helpEmeny.dataset.click = 'false'
+        helpSettings.style.cursor= 'auto';
+      }
 
     })
   })
