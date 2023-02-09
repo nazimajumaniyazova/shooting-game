a9913189b96efc32b5005d02914778f816f16b4b
AntonFio <anton_fiolkovski@mail.ru>
2023-02-10 02:00:12 +0300

    feat: transfer block modal to singlModals.ts

--- src/type/settingModal.ts
+++ src/type/settingModal.ts
@@ -1,3 +1,43 @@
+const renderSettings = () => {
+  const html = `
+  <div class="wrapper-modal__settings">
+    <div class="player">
+      <audio class="audio" src="./catalog-img/audio/one.mp3"></audio>
+      <div class="box-player-wrapper">
+        <div class="box-player">
+          <div class="btn play-pause"><img class="img-play-pause" src="./catalog-img/play.png" alt=""></div>
+          <div class="volume-box">
+            <div class="img-volume"><img class="reg-volume" src="./catalog-img/volume.png" alt=""></div>
+            <input class="volmue" type="range" value="0.3" min="0" max="1" step="0.1">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="settings-one">
+      <span class="settings-text">Settings №1</span>
+      <div class="switch-btn switch-one"></div>
+    </div>
+    <div class="settings-two">
+      <span class="settings-text">Settings №2</span>
+      <div class="switch-btn switch-two"></div>
+    </div>
+    <div class="settings-three">
+      <span class="settings-text">Settings №3</span>
+      <div class="switch-btn switch-three"></div>
+    </div>
+  </div>
+`
+  const saction = <HTMLElement>document.createElement('div');
+  saction.classList.add('wrapper-modal')
+  saction.classList.add('active')
+  saction.innerHTML = html;
+  document.body.appendChild(saction);
+  const sactionSettings = <HTMLElement>document.createElement('div');
+  sactionSettings.classList.add('settings')
+  document.body.appendChild(sactionSettings);
+}
+renderSettings()
+
 const btnSettings = document.querySelector('.settings') as HTMLElement
 // const player = document.querySelector('.player') as HTMLElement
 const imgPlayPause = document.querySelector('.img-play-pause') as HTMLImageElement;
@@ -7,36 +47,6 @@ const btnPlay = document.querySelector('.play-pause') as HTMLElement
 const switchBtn = document.querySelectorAll('.switch-btn');
 
 audio.volume = 0.3
-
-// const renderSettings = async () => {
-//  const html = `
-//  <div class="wrapper-modal active">
-//  <div class="wrapper-modal__settings">
-
-//    <div class="player">
-//      <audio class="audio" autoplay="autoplay" src="#"></audio>
-
-//      <div class="box-player-wrapper">
-//        <div class="box-player">
-//          <div class="btn play"><img class="img-play-pause" src="./assets/play.png" alt=""></div>
-
-//          <div class="volume-box">
-//            <div class="img-volume"><img class="reg-volume" src="./assets/volume.png" alt=""></div>
-//            <input class="volmue" type="range" value="0.3" min="0" max="1" step="0.1">
-//          </div>
-
-//        </div>
-//      </div>
-//    </div>
-
-//  </div>
-// </div>
-// `
-//   const saction = <HTMLElement>document.createElement('div');
-//   saction.classList.add('wrapper-modal')
-//     saction.innerHTML = html;
-//     document.body.appendChild(saction);
-// }
 
 
 volume.addEventListener('input', () => {
@@ -83,4 +93,6 @@ switchBtn.forEach(item => {
     }
   })
 })
+
+export { };
 
