42b1b2f6a5aff8e22e02aeaf4da81f448a82aebc
AntonFio <anton_fiolkovski@mail.ru>
2023-02-14 17:53:38 +0300

    feat: add valid form

--- src/type/loginModal.ts
+++ src/type/loginModal.ts
@@ -23,21 +23,21 @@ const renderWrapperLogin = () => {
       <form class="wrapper__form-fiel">
         <div class="form-field condition-name active">
           <label for="name">Name</label>
-          <input type="text" class="username" name="name" required="" autofocus="">
+          <input type="text" class="input username" name="name" required="" autofocus="">
         </div>
 
         <div class="form-field">
           <label for="email-signup">Email</label>
-          <input type="email" class="email-signup" name="email" autocomplete="email" required="">
+          <input type="email" class="input email-signup" name="email" autocomplete="email" required="">
         </div>
 
         <div class="form-field">
           <label for="password-signup">Password</label>
-          <input type="password" class="password-signup" name="password" autocomplete="new-password" required="" minlength="5" aria-autocomplete="list">
+          <input type="password" class="input password-signup" name="password" autocomplete="new-password" required="" minlength="5" aria-autocomplete="list">
         </div>
 
         <div class="form-field condition-password active">
-          <label for="password-confirm">Confirm password</label> <input type="password" id="password-confirm" name="password-confirm" autocomplete="new-password" required="" minlength="5">
+          <label for="password-confirm">Confirm password</label> <input type="password" class="input password-confirm" name="password-confirm" autocomplete="new-password" required="" minlength="5">
         </div>
 
         <button class="btn-active btn-reset form__btn"  data="sign-in" type="submit">Sign in</button>
@@ -62,6 +62,7 @@ const formBtn = document.querySelector('.form__btn') as HTMLButtonElement
 const email = document.querySelector(".email-signup") as HTMLInputElement;
 const username = document.querySelector(".username") as HTMLInputElement;
 const pass = document.querySelector(".password-signup") as HTMLInputElement;
+const passConfirm = document.querySelector(".password-confirm") as HTMLInputElement;
 const wrapperLogin = document.querySelector('.wrapper-login') as HTMLElement
 
 headerUser?.addEventListener('click', () => {
@@ -74,11 +75,22 @@ wrapperLogin.addEventListener('click', (e) => {
   }
 })
 
-// interface users {
-//   userName: string,
-//   email: string,
-//   password: string,
-// }
+interface users {
+  username: string,
+  email: string,
+  password: string,
+}
+
+function resetstyles() {
+  email.value = ''
+  username.value = ''
+  pass.value = ''
+  passConfirm.value = ''
+  email.classList.remove('valid')
+  username.classList.remove('valid')
+  pass.classList.remove('valid')
+  passConfirm.classList.remove('valid')
+}
 
 signOn.addEventListener('click', () => {
   signIn.classList.remove('active')
@@ -87,6 +99,7 @@ signOn.addEventListener('click', () => {
   signOn.classList.add('active')
   formBtn.innerHTML = 'Sign on'
   formBtn.setAttribute('data', 'sign-on')
+  resetstyles()
 })
 
 signIn.addEventListener('click', () => {
@@ -96,23 +109,67 @@ signIn.addEventListener('click', () => {
   signOn.classList.remove('active')
   formBtn.innerHTML = 'Sign in'
   formBtn.setAttribute('data', 'sign-in')
+  resetstyles()
 })
 
-function conditionSignOn() {
+const nameReg = /^[a-z0-9_-]{3,16}$/;
+const emailReg = /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
 
-  const user = {
-    email: email.value ,
+function conditionSignOn() {
+  const user: users  = {
+    email: email.value,
     username: username.value,
     password: pass.value,
   }
 
   const json = JSON.stringify(user);
   localStorage.setItem(username.value, json)
-  console.log(localStorage.getItem('username'))
 }
 
-formBtn.addEventListener('click', () => {
+formBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const emailIn = email.value.trim();
+  const usernameIn = username.value.trim()
+  const passIn = pass.value.trim()
+  const passConfirmIn = passConfirm.value.trim()
   if (formBtn.getAttribute('data') === 'sign-on') {
+    
+  if (!emailReg.test(emailIn)) {
+    email.style.border = '1px solid #f00'
+    email.classList.remove('valid')
+  } else {
+    email.classList.add('valid')
+    email.style.border = '1px solid #000'
+    }
+
+    if (!nameReg.test(usernameIn) && usernameIn === '') {
+      username.style.border = '1px solid #f00'
+      username.classList.remove('valid')
+    } else {
+      username.style.border = '1px solid #000'
+      username.classList.add('valid')
+    }
+
+    if (passIn != '' && passConfirmIn != '' && passIn === passConfirmIn) {
+      pass.classList.add('valid')
+      passConfirm.classList.add('valid')
+      pass.style.border = '1px solid #000'
+      passConfirm.style.border = '1px solid #000'
+    } else {
+      pass.style.border = '1px solid #f00'
+      passConfirm.style.border = '1px solid #f00'
+      pass.classList.remove('valid')
+      passConfirm.classList.remove('valid')
+    }
+  }
+
+  if (email.classList.contains('valid') &&
+    username.classList.contains('valid') &&
+    pass.classList.contains('valid') &&
+    passConfirm.classList.contains('valid')) {
     conditionSignOn()
+    resetstyles()
+    alert('вы зарегистрировались')
+    wrapperLogin.classList.add('active')
   }
 })
